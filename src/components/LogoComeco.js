01119559ee316b19722dbdc083e991f424dbb735
Vinicius <vianaxd99@gmail.com>
2022-07-23 20:33:06 -0300

    algumas componentizacoes

---
+++ src/components/LogoComeco.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import logo from "../assets/logo.png";
+
+export default function LogoComeco() {
+    return (
+        <div class="logo-comeco">
+            <img src={logo} />
+            <h1>ZapRecall</h1>
+        </div>
+    )
+}
--- src/components/TelaComeco.js
+++ src/components/TelaComeco.js
@@ -3,14 +3,12 @@ import React from 'react';
 import "../css/style.css";
 
 import logo from "../assets/logo.png";
+import LogoComeco from './LogoComeco';
 
 export default function TelaComeco({mudaTela}) {
     return (
         <>
-            <div class="logo-comeco">
-                <img src={logo} />
-                <h1>ZapRecall</h1>
-            </div>
+            <LogoComeco />
             <div class="botao-comecar" onClick={mudaTela}><p>Iniciar Recall!</p></div>
         </>
     );
